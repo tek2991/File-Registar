20003a08ae48b79fce43703f7127fbfef8f02ffb
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-10-18 13:28:31 +0530

    Updated application logo

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,6 +1,10 @@
-<a href="/">
-    <svg class="w-16 h-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
-        <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
-    </svg>
-</a>
+<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 100 100"
+    style=" fill:#000000;">
+    <polygon fill="#4b4dff" points="74,82 26,82 26,20 61,20 74,33"></polygon>
+    <polygon fill="#edf7f5" points="73,38 56,38 56,21 60,21 60,34 73,34"></polygon>
+    <path fill="#4343bf" d="M77,85H23V17h40.313L77,31.827V85z M29,79h42V34.173L60.687,23H29V79z"></path>
+    <rect width="20" height="4" x="40" y="43" fill="#edf7f5"></rect>
+    <rect width="20" height="4" x="40" y="54" fill="#edf7f5"></rect>
+    <rect width="20" height="4" x="40" y="64" fill="#edf7f5"></rect>
+    <polygon fill="#3abcf8" points="71,89 20,89 20,13 60,13 60,23 30,23 30,79 71,79"></polygon>
+</svg>
